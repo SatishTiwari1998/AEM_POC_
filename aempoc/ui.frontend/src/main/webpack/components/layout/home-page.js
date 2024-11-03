53af58ceba0af410d58e2e796f2387e1ce81f40c
Satish Tiwari <96436404+SatishTiwari1998@users.noreply.github.com>
2024-11-04 00:52:05 +0530

    Login Setup & Dynamic user setup

--- aempoc/ui.frontend/src/main/webpack/components/layout/home-page.js
+++ aempoc/ui.frontend/src/main/webpack/components/layout/home-page.js
@@ -2,13 +2,13 @@ import { blogPosts } from "../blogPosts";
 import { showSpinner, hideSpinner } from "../spinner";
 
 $(document).ready(function () {
-    // Spinner Control
     homePage.initSpinner();
     setTimeout(() => {
         homePage.hideSpinner();
     }, 1500);
-    
-    // Initialize Blog Page if on Home Page
+
+    homePage.setUser();
+
     if ($("#poc-home").length) {
         homePage.init();
     }
@@ -29,11 +29,7 @@ const homePage = {
 
     displayRandomBlogs: function (posts, count = 6) {
         const container = $(".home-page-featured #dynamic-blogs-container");
-
-        // Shuffle and slice posts
         const shuffledPosts = posts.sort(() => 0.5 - Math.random()).slice(0, count);
-
-        // Render each post
         $.each(shuffledPosts, (index, post) => {
             const blogCard = `
                 <li class='item' style="background-image: url('${post.blogThumbnail}')">
@@ -46,5 +42,29 @@ const homePage = {
             `;
             container.append(blogCard);
         });
+    },
+
+    setUser: function(){
+        const userData = JSON.parse(localStorage.getItem("user"));
+        const authorProfile = $('.author-profile');
+        const name = authorProfile.find('.profile-name .name');
+        const uniqueId = authorProfile.find('.profile-name .unique-id');
+        const profileImg = authorProfile.find('.profile-image img');
+        const idGenerator = $('.poc_idGenerator');
+        
+        if (userData && userData.loggedIn) {
+            name.text(userData.details.name);
+            uniqueId.text(userData.details.id);
+            if(idGenerator.length>0){
+                idGenerator.hide();
+            }
+        } else {
+            name.text("Guest User");
+            uniqueId.text("--");
+            if(idGenerator.length>0){
+                idGenerator.show();
+            }
+        }
     }
+    
 };
